2e2af6b9f4789da0ce3d28c31207db86df2d16d1
sky <sky@netgowifi.com>
2021-09-07 00:21:40 +0300

    add certmanager

--- k8s/ingress-controller.sh
+++ k8s/ingress-controller.sh
@@ -1,3 +1,9 @@
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
 helm install ingress-nginx ingress-nginx/ingress-nginx
+
+##cert-manager
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
+helm install cert-manager jetstack/cert-manager
+kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.5.3/cert-manager.crds.yaml
